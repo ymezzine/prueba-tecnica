9f470e10786f788764794a5808cf6f2d13fa3367
LAPTOP-CN5L8M51\yacin <yacine.mezzine@outlook.com>
2023-04-28 15:25:01 +0100

    add stack

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+var express = require('express')
+var bodyParser = require('body-parser')
+
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Prueba tecnica API</h1>');
+});
+
+
+app.listen(4800, function () {
+  console.info("API Started")
+});
+
+
+module.exports = app
